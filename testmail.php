cb09c18c8bdb6ac2017e638c72185f6ac42f79e4
Robiul Islam Robi <dmrobi89@gmail.com>
2015-10-21 02:11:43 +0600

    testmail.php added

---
+++ testmail.php
@@ -0,0 +1,6 @@
+<?php
+$to = "dmrobi89@gmail.com";
+$subject = "Email Verification";
+$txt = "Please use this link to activate your account.";
+$headers = "From: LogicPen@example.com";
+mail($to, $subject, $txt, $headers);
